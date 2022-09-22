d0dc39c5139610b9c6b92783b1988002e4e45554
Salitha Chathuranga <salithachathuranga94@gmail.com>
2022-09-22 07:14:53 +0530

    Fix service layer tests

--- src/test/java/com/rest/order/OrderServiceUnitTest.java
+++ src/test/java/com/rest/order/OrderServiceUnitTest.java
@@ -2,15 +2,19 @@ package com.rest.order;
 
 import com.rest.order.exceptions.OrderNotFoundException;
 import com.rest.order.models.Order;
+import com.rest.order.repositories.OrderRepository;
 import com.rest.order.services.OrderService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -19,7 +23,10 @@ import static org.mockito.Mockito.*;
 public class OrderServiceUnitTest {
 
     @Mock
-    private OrderService orderService;
+    OrderRepository orderRepository;
+
+    @InjectMocks
+    OrderService orderService;
 
     @BeforeEach
     public void setup() {
@@ -30,29 +37,29 @@ public class OrderServiceUnitTest {
     public void testGetOrdersList() {
         Order order1 = new Order(8L, "ben", 80.0, 5);
         Order order2 = new Order(9L, "kevin", 70.0, 2);
-        when(orderService.getOrders()).thenReturn(Arrays.asList(order1, order2));
-        assertEquals(orderService.getOrders().size(), 2);
-        assertEquals(orderService.getOrders().get(0).getBuyer(), "ben");
-        assertEquals(orderService.getOrders().get(1).getBuyer(), "kevin");
-        assertEquals(orderService.getOrders().get(0).getPrice(), 80.0);
-        assertEquals(orderService.getOrders().get(1).getPrice(), 70.0);
-        assertNotEquals(orderService.getOrders().get(0).getBuyer(), null);
-        assertNotEquals(orderService.getOrders().get(1).getBuyer(), null);
+        when(orderRepository.findAll()).thenReturn(Arrays.asList(order1, order2));
+        List<Order> orderList = orderService.getOrders();
+        assertEquals(orderList.size(), 2);
+        assertEquals(orderList.get(0).getBuyer(), "ben");
+        assertEquals(orderList.get(1).getBuyer(), "kevin");
+        assertEquals(orderList.get(0).getPrice(), 80.0);
+        assertEquals(orderList.get(1).getPrice(), 70.0);
     }
 
     @Test
     public void testGetOrderById() {
         Order order = new Order(7L, "george", 60.0, 6);
-        when(orderService.getOrderById(7L)).thenReturn(order);
-        assertEquals(orderService.getOrderById(7L).getBuyer(), "george");
-        assertEquals(orderService.getOrderById(7L).getPrice(), 60.0);
-        assertNotEquals(orderService.getOrderById(7L).getBuyer(), null);
+        when(orderRepository.findById(7L)).thenReturn(Optional.of(order));
+        Order orderById = orderService.getOrderById(7L);
+        assertNotEquals(orderById, null);
+        assertEquals(orderById.getBuyer(), "george");
+        assertEquals(orderById.getPrice(), 60.0);
     }
 
 
     @Test
     public void testGetInvalidOrderById() {
-        when(orderService.getOrderById(17L)).thenThrow(new OrderNotFoundException("Order Not Found with ID"));
+        when(orderRepository.findById(17L)).thenThrow(new OrderNotFoundException("Order Not Found with ID"));
         Exception exception = assertThrows(OrderNotFoundException.class, () -> {
             orderService.getOrderById(17L);
         });
@@ -63,9 +70,9 @@ public class OrderServiceUnitTest {
     public void testCreateOrder() {
         Order order = new Order(12L, "john", 90.0, 6);
         orderService.createOrder(order);
-        verify(orderService, times(1)).createOrder(order);
+        verify(orderRepository, times(1)).save(order);
         ArgumentCaptor<Order> orderArgumentCaptor = ArgumentCaptor.forClass(Order.class);
-        verify(orderService).createOrder(orderArgumentCaptor.capture());
+        verify(orderRepository).save(orderArgumentCaptor.capture());
         Order orderCreated = orderArgumentCaptor.getValue();
         assertNotNull(orderCreated.getId());
         assertEquals("john", orderCreated.getBuyer());
@@ -74,10 +81,11 @@ public class OrderServiceUnitTest {
     @Test
     public void testDeleteOrder() {
         Order order = new Order(13L, "simen", 120.0, 10);
+        when(orderRepository.findById(13L)).thenReturn(Optional.of(order));
         orderService.deleteOrderById(order.getId());
-        verify(orderService, times(1)).deleteOrderById(order.getId());
+        verify(orderRepository, times(1)).deleteById(order.getId());
         ArgumentCaptor<Long> orderArgumentCaptor = ArgumentCaptor.forClass(Long.class);
-        verify(orderService).deleteOrderById(orderArgumentCaptor.capture());
+        verify(orderRepository).deleteById(orderArgumentCaptor.capture());
         Long orderIdDeleted = orderArgumentCaptor.getValue();
         assertNotNull(orderIdDeleted);
         assertEquals(13L, orderIdDeleted);
